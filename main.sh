76e41b3f1706700a4870570d5c3b908633da2fd0
Ruhi Mahadeshwar <61055561+imruhi@users.noreply.github.com>
2026-05-28 16:37:41 +0200

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# step one get topos texts (takes around 1.5 hours)
+## step one get topos texts (takes around 1.5 hours)
 # echo "########## Extracting topos texts ##########"
 # python data_gathering/utils/topos_extraction.py
 
@@ -16,7 +16,8 @@
 # echo "########## Filtering using topic modeling ##########"
 # python data_gathering/utils/topic_model_filtering.py
 
-# step five train+eval model
+# # step five train+eval model
 echo "########## Finetuning/Evaluating ##########"
-# tensorboard --logdir /home/imruhi/Documents/RiskPerceptionSeafare/classification/mmBERT-base_finetuned_16_12/checkpoints/runs
-python -m classification.finetune
+# # tensorboard --logdir /home/imruhi/Documents/RiskPerceptionSeafare/classification/mmBERT-base_finetuned_16_12/checkpoints/runs
+# python -m classification.finetune
+python -m classification.logistic_finetune
